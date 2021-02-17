201e879824203470b74769255e777e22b0d90bbd
Claudio Scheer <claudioscheer@protonmail.com>
2021-02-17 10:17:17 -0300

    Fit model

--- assessment-2/src/cifar10_cnn.py
+++ assessment-2/src/cifar10_cnn.py
@@ -11,7 +11,7 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 import os
 
-batch_size = 32
+batch_size = 64
 num_classes = 5
 epochs = 100
 data_augmentation = False
@@ -31,16 +31,7 @@ train_ids = np.genfromtxt(
 train_ids = {x[0]: x[1] for x in train_ids}
 
 x_train, y_train = get_train_dataset(train_dataset, train_ids)
-
-# The data, split between train and test sets:
-# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-# print("x_train shape:", x_train.shape)
-# print(y_train[0], "train samples")
-# print(x_test.shape[0], "test samples")
-
-# Convert class vectors to binary class matrices.
 y_train = tf.keras.utils.to_categorical(y_train, num_classes)
-# y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding="same", input_shape=x_train.shape[1:]))
@@ -64,25 +55,25 @@ model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation("softmax"))
 
-# initiate RMSprop optimizer
 opt = tf.keras.optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
 
-# Let's train the model using RMSprop
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-x_train = x_train.astype("float32")
-# x_test = x_test.astype("float32")
-x_train /= 255
-# x_test /= 255
+model.fit(
+    x_train,
+    y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    # validation_data=(x_test, y_test),
+    shuffle=True,
+)
 
-# Save model and weights
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 model_path = os.path.join(save_dir, model_name)
 model.save(model_path)
 print("Saved trained model at %s " % model_path)
 
-# Score trained model.
 scores = model.evaluate(x_train, y_train, verbose=1)
 print("Test loss:", scores[0])
 print("Test accuracy:", scores[1])
